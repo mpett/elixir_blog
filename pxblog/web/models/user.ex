91d90835d91d7ca678b69c6c92815fe13af856c1
Martin Pettersson <kontakt@martinpettersson.se>
2017-07-27 15:45:48 +0200

    Added salt and hash for passwords.

--- pxblog/web/models/user.ex
+++ pxblog/web/models/user.ex
@@ -1,5 +1,6 @@
 defmodule Pxblog.User do
   use Pxblog.Web, :model
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :username, :string
@@ -23,7 +24,12 @@ defmodule Pxblog.User do
   end
 
   defp hash_password(changeset) do
-    changeset
-    |> put_change(:password_digest, "ABCDE")
+    if password = get_change(changeset, :password) do
+      changeset
+      |> put_change(:password_digest, hashpwsalt(password))
+    else
+      changeset
+    end
+
   end
 end
