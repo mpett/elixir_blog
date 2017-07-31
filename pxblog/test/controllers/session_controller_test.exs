43c4ec1150d6ee2ff00b38a9567a5b62eb87aaa0
Martin Pettersson <kontakt@martinpettersson.se>
2017-07-31 10:06:30 +0200

    Started on session controller tests.

---
+++ pxblog/test/controllers/session_controller_test.exs
@@ -0,0 +1,10 @@
+defmodule Pxblog.SessionControllerTest do
+  use Pxblog.ConnCase
+  alias Pxblog.User
+
+  setup do
+    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
+    |> Repo.insert
+    {:ok, conn: build_conn()}
+  end
+end
