baed8408487c888e2e178ba50a749b637e5d4cce
Martin Pettersson <kontakt@martinpettersson.se>
2017-07-31 14:06:42 +0200

    Completed layout tests.

--- pxblog/test/views/layout_view_test.exs
+++ pxblog/test/views/layout_view_test.exs
@@ -15,9 +15,17 @@ defmodule Pxblog.LayoutViewTest do
     assert LayoutView.current_user(conn)
   end
 
-  test "current_user returns nothing if there is no user in the sessin", %{conn: conn} do
+  test "current_user returns nothing if there is no user in the session", %{conn: conn} do
     user = Repo.get_by(User, %{username: "test"})
     conn = delete conn, session_path(conn, :delete, user)
     refute LayoutView.current_user(conn)
+  end
+
+  test "deletes the user session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: test})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Signed out succesfully!"
+    assert redirected_to(conn) == page_path(conn, :index)
   end
 end
