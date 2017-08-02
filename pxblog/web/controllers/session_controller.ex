86fc40cc2cae178e2e05d56de37be20f0fce281f
Martin Pettersson <kontakt@martinpettersson.se>
2017-08-02 14:32:22 +0200

    Added some basic error handling for failed sign ins.

--- pxblog/web/controllers/session_controller.ex
+++ pxblog/web/controllers/session_controller.ex
@@ -1,7 +1,7 @@
 defmodule Pxblog.SessionController do
   use Pxblog.Web, :controller
 
-  import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
   plug :scrub_params, "user" when action in [:create]
 
@@ -11,9 +11,38 @@ defmodule Pxblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  defp failed_login(conn) do
+    dummy_checkpw()
+    conn
+    |> put_session(:current_user, nil)
+    |> put_flash(:error, "We are sorry. This is an invalid combination of your user name and password. Maybe next time around?")
+    |> redirect(to: page_path(conn, :index))
+    |> halt()
+  end
+
+  defp sign_in(user, _password, conn) when is_nil(user) do
+    failed_login(conn)
+  end
+
+  defp sign_in(user, password, conn) do
+    if checkpw(password, user.password_digest) do
+      conn
+      |> put_session(:current_user, %{id: user.id, username: user.username})
+      |> put_flash(:info, "Congratulations and Welcome! The sign in was successful.")
+      |> redirect(to: page_path(conn, :index))
+    else
+      failed_login(conn)
+    end
+  end
+
+  def create(conn, %{"user" => %{"username" => username, "password" => password}})
+  when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
